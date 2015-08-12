d7a2a50a5d36d3da5033c92db90e11c1c91bf437
ziyad <faabesoft@live.com>
2015-08-12 16:45:53 +0400

    first commit

---
+++ src/JustBlog.Core/Object/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace JustBlog.Core.Object
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string UrlSlug { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ src/JustBlog.Core/Object/Tag.cs
@@ -0,0 +1,6 @@
+﻿namespace JustBlog.Core.Object
+{
+    public class Tag
+    {
+    }
+}
